d582fc34edf5ed079fd763b7e57d7e1903e29039
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-25 09:32:55 +0100

    changes

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Use your own credentials JSON file obtained from Google Cloud Platform
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('secrets.json', scope)
 client = gspread.authorize(creds)
 
 # Access a specific Google Sheet by its URL or name
