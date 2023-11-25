c316c05570a781179cc8e611f6c7f15e4c9cd4e9
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-25 09:19:06 +0100

    changes

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -1,14 +1,3 @@
-import streamlit as st
-import pandas as pd
-
-# Dispay Title and Description
-st.title("Fridge")
-st.markdown("Enter the product below")
-
-# .streamlit/secrets.toml
-[connections.gsheets]
-spreadsheet = "https://docs.google.com/spreadsheets/d/1ipfG6cDGUQjfhl6DjAWXfFaPfs05URC_tM64Dfz7Wts"
-
 # streamlit_app.py
 
 import streamlit as st
