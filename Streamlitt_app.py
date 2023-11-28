b27542fb156384e80993235c7194732577b790ab
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-28 09:48:20 +0100

    Changes

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -1,13 +1,6 @@
-# streamlit_app.py
-
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+# Dispay Title and Description
+st.title("Fridge")
+st.markdown("Enter the product below")
