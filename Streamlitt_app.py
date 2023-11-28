aa01c9921ff82ac72affeb0159fe702595bfafe6
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-28 10:36:46 +0100

    change

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -1,6 +1,11 @@
-import streamlit as st
-import pandas as pd
+import pymysql
 
-# Dispay Title and Description
-st.title("Fridge")
-st.markdown("Enter the product below")
+# Database connection details 
+db_host = p274783438042-nq2c2m@gcp-sa-cloud-sql.iam.gserviceaccount.com
+db_user = mysql-csgroupproject8-4
+db_password = +/iHlkkQ=YxD#Ip7
+db_name = cs-group-project-8-4:europe-west6:mysql-csgroupproject8-4
+
+# Connect to the cloud database
+connection = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+cursor = connection.cursor()
