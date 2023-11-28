576efd2d3f555101e0c886c6ce072e237117e28e
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-11-28 11:19:45 +0100

    change

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -4,8 +4,10 @@ import pymysql
 db_host = "34.65.8.211"
 db_user = "mysql-csgroupproject8-4"
 db_password = "+/iHlkkQ=YxD#Ip7"
-db_name = "cs-group-project-8-4"
+db_name = "products-"
 
 # Connect to the cloud database
 connection = pymysql.connect(host=db_host, user=db_user, password=db_password, database=db_name)
 cursor = connection.cursor()
+
+connection.close()
