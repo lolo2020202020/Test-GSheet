9e98a17891d997393c7f3b24df71bf9040645110
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-12-02 19:55:52 +0100

    changes

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -1,4 +1,4 @@
-
+import st-gsheets-connection as st
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
