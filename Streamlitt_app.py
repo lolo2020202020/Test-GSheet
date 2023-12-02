ed7a0beec9ed2584e271e2af9bb25abf3c3bb3cd
lorenzolonni <147751706+lolo2020202020@users.noreply.github.com>
2023-12-02 19:47:23 +0100

    changes

--- Streamlitt_app.py
+++ Streamlitt_app.py
@@ -1,21 +1,9 @@
+pip install st-gsheets-connection
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit#gid=1453155026"
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1], worksheet="1585633377")
+data = conn.read(spreadsheet=url, usecols=[0, 1])
 st.dataframe(data)
-
-# Old code with streamlit-gsheets
-gc = GSheetsConnection()
-df = gc.read_sheet("Sheet1")
-
-# New code with shillelagh
-import shillelagh
-
-gc = shillelagh.connect()
-df = gc.read_sheet("Sheet1")
-
-
